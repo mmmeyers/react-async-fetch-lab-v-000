322042fac25d9ee12890a8ac8e0d4091dcc90853
Marsi Meyers-Miyashita <marsi286@gmail.com>
2019-09-19 02:25:38 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.state.peopleInSpace.map(person, id) => <h1 key=person.name)}
+        {this.state.peopleInSpace.map(person, id) => <h1 key={id}person.name)}
       </div>
       )
   }
