c34cd7f8a2f4e9084fc70bc3ba1ff227abe93301
Marsi Meyers-Miyashita <marsi286@gmail.com>
2019-09-19 02:24:03 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -18,7 +18,9 @@ class App extends Component {
   componentDidMount() {
     fetch( http://api.open-notify.org/astros.json)
     .then(response => response.json())
-    .then(data => )
+    .then(data =>  {
+      this.setState({})
+    })
   }
   
   
